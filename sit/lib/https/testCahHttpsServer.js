c02234754922527eabdd0936b65a5c097edc531a
Pommy Mac <tm_git@93m.co>
2021-01-27 16:44:20 +0100

    Refactored https.createServer to match tls.createServer

--- sit/lib/https/testCahHttpsServer.js
+++ sit/lib/https/testCahHttpsServer.js
@@ -4,15 +4,8 @@ module.exports = async ({ port = 45231, ...options } = {}) => {
   const srv = await https.createServer(
     options,
     (req, res) => {
-      const data = []
-
-      req.on('data', (chunk) => {
-        data.push(chunk)
-      })
-      req.on('end', () => {
-        res.writeHead(200, { 'Content-Type': 'application/html' })
-        res.end()
-      })
+      res.writeHead(200, { 'Content-Type': 'application/html' })
+      res.end()
     }
   )
 

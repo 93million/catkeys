67648ab37a46223e73f76fbff1104d3ed2c248ff
Pommy Mac <tm_git@93m.co>
2021-02-06 05:42:30 +0100

    Added examples for json-socket

---
+++ example/json-socket/request.js
@@ -0,0 +1,17 @@
+const { tls } = require('catkeys')
+const JsonSocket = require('json-socket')
+
+const connect = async () => {
+  const port = 9838
+  const host = '127.0.0.1'
+  const socket = new JsonSocket(await tls.connect({ host, port }))
+
+  socket.on('connect', () => {
+    socket.sendMessage({ a: 5, b: 7 })
+    socket.on('message', (message) => {
+      console.log('The result is: ' + message.result)
+    })
+  })
+}
+
+connect()
---
+++ example/json-socket/serve.js
@@ -0,0 +1,19 @@
+const { tls } = require('catkeys')
+const JsonSocket = require('json-socket')
+
+const serve = async () => {
+  const port = 9838
+  const server = await tls.createServer()
+
+  server.listen(port)
+  server.on('secureConnection', (socket) => {
+    socket = new JsonSocket(socket)
+    socket.on('message', function (message) {
+      const result = message.a + message.b
+
+      socket.sendEndMessage({ result: result })
+    })
+  })
+}
+
+serve()

a2e9eee9f3ac274e2623adb69e2bcdbdedb0b4d5
Pommy Mac <tm_git@93m.co>
2021-02-07 21:14:20 +0100

    Outputting text describing that stdin on server/client will be sent over the socket and output on stdout

--- examples/tls/serve.js
+++ examples/tls/serve.js
@@ -3,6 +3,12 @@ const { tls } = require('catkeys')
 const serve = async () => {
   (await tls.createServer(
     (socket) => {
+      console.log('Client connected.')
+      console.log('Text entered here will be send to stdout on the client')
+
+      socket.write('Connected to server.\n')
+      socket.write('Text entered here will be send to stdout on the server\n')
+
       socket.pipe(process.stdout)
       process.stdin.pipe(socket)
     }

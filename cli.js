18a59f60a4a443f9defbcbad760e206239aeb77f
Pommy Mac <tm_git@93m.co>
2021-02-13 14:40:02 +0100

    Created extract command to extract key contents

--- cli.js
+++ cli.js
@@ -4,6 +4,14 @@ const locateKeysDir = require('./lib/locateKeysDir')
 const createKey = require('./lib/createKey')
 const yargs = require('yargs')
 const config = require('./config')
+const extractFilesFromKey = require('./lib/createKey/extractFilesFromKey')
+const path = require('path')
+const fileExists = require('./lib/fileExists')
+const fs = require('fs')
+const { promisify } = require('util')
+
+const mkdir = promisify(fs.mkdir)
+const rmdir = promisify(fs.rmdir)
 
 const main = async () => {
   // eslint-disable-next-line no-unused-expressions
@@ -49,7 +57,59 @@ const main = async () => {
         }
       }
     )
+    .command(
+      'extract-key [path]',
+      'extract PEM certs and keys from a .catkey file',
+      (yargs) => {
+        yargs
+          .positional(
+            'path',
+            {
+              description: 'path to key file',
+              type: 'string'
+            }
+          )
+          .option({
+            'output-dir': {
+              alias: 'o',
+              description: 'path to the directory to output the key files'
+            }
+          })
+          .demandOption('path')
+      },
+      async (argv) => {
+        const pathParsed = path.parse(path.resolve(argv.path))
+        const keyPath = path.resolve(argv.path)
+        const outputDir = path.resolve(
+          argv['output-dir'] || process.cwd(),
+          pathParsed.name
+        )
+        const outputDirExists = await fileExists(outputDir)
+
+        if (!await fileExists(keyPath)) {
+          console.error(`${keyPath} does not exist`)
+
+          return
+        }
+
+        try {
+          if (!outputDirExists) {
+            await mkdir(outputDir)
+          }
+
+          await extractFilesFromKey(keyPath, outputDir, { exclude: ['.srl'] })
+        } catch (e) {
+          console.error('Failed to extract key:', e.message)
+
+          if (!outputDirExists) {
+            await rmdir(outputDir)
+          }
+        }
+      }
+    )
     .demandCommand()
+    .strict()
+    .help()
     .argv
 }
 

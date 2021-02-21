f788cd683cfe29a518500aac41f321efbcdd0ae6
Pommy Mac <tm_git@93m.co>
2021-02-21 21:49:41 +0100

    Created SIT tests for cli command extract-key

--- sit/catkeys.test.js
+++ sit/catkeys.test.js
@@ -2,7 +2,19 @@
 
 const path = require('path')
 const loadKey = require('../lib/loadKey')
-const { testCatkeysDir } = require('./filepaths')
+const {
+  cliCmd,
+  testCatkeysDir,
+  testCatkeysExtractionDir
+} = require('./filepaths')
+const childProcess = require('child_process')
+const { promisify } = require('util')
+const fileExists = require('../lib/fileExists')
+const fs = require('fs')
+
+const execFile = promisify(childProcess.execFile)
+const mkdir = promisify(fs.mkdir)
+const stat = promisify(fs.stat)
 
 describe(
   'Key generation',
@@ -20,6 +32,34 @@ describe(
           cert: expect.any(Buffer),
           key: expect.any(Buffer)
         })
+      }
+    )
+
+    test(
+      'should extract key into components',
+      async () => {
+        if (!await fileExists(testCatkeysExtractionDir)) {
+          await mkdir(testCatkeysExtractionDir)
+        }
+
+        await execFile(
+          'node',
+          [
+            cliCmd,
+            'extract-key',
+            '-o',
+            testCatkeysExtractionDir,
+            path.resolve(testCatkeysDir, 'server.catkey')
+          ]
+        )
+        const stats = await Promise.all(
+          ['ca-crt.pem', 'ca-key.pem', 'crt.pem', 'key.pem']
+            .map((file) => stat(
+              path.resolve(testCatkeysExtractionDir, 'server', file)
+            ))
+        )
+
+        expect(stats.every(({ size }) => (size > 0))).toBe(true)
       }
     )
   }
